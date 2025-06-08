4eb7c8619e52a1e0aa2e5347c7e9cf4bf0dd1414
Austine247-droid <ugberaeseoa@gmail.com>
2025-06-08 20:05:47 +0000

    Updated files

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -4,3 +4,38 @@ export interface CardProps {
 }
 
 
+// Add shared interfaces here
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface CardProps {
+  title: string;
+  content: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  size?: 'small' | 'medium' | 'large';
+  shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+}
+
+
+export interface PostProps {
+  id?: number;
+  title: string;
+  content: string;
+  userId: number;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+  };
+}
