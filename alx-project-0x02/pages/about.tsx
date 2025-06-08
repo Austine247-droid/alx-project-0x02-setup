1c4a03a7161e6cabf3e3e4304ba9d9edcfbf6764
Austine247-droid <ugberaeseoa@gmail.com>
2025-06-08 20:10:39 +0000

    Updated files

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Header from '@/components/layout/Header';
+import Button from '@/components/common/Button';
 
-const about = () => {
+export default function About() {
   return (
-    <div>
-      <h1 className='flex justify-center items-center text-green-400 '>This is the About page</h1>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <main className="p-8 space-y-6">
+        <h1 className="text-3xl font-bold">About Us</h1>
 
-export default about
+        <div className="flex gap-4 flex-wrap">
+          <Button label="Small Button" size="small" shape="rounded-sm" />
+          <Button label="Medium Button" size="medium" shape="rounded-md" />
+          <Button label="Large Button" size="large" shape="rounded-full" />
+        </div>
+      </main>
+    </>
+  );
+}
